3f2fdea26f9c4e5e662fc9470a3c6fc5856ac11c
Andreza Feu <andrezafeu@gmail.com>
2016-04-27 15:42:24 -0400

    Save searched artist by id and show album list after clicking  on artist

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 $(document).on("ready", function () {
 
 	$(".js-search").on("click", function (event) {
@@ -17,13 +16,47 @@ $(document).on("ready", function () {
 
 			success: function(data) {
 				console.log("Success");
-				console.log(data.artists.items[0].name);
-				console.log(data.artists.items[0].images);
+				console.log(data)
+				// console.log(data.artists.items[0].name);
+				// console.log(data.artists.items[0].images);
 				displayInfo(data.artists.items);
-				console.log(data.artists.items[0].images[0].url);
+				// console.log(data.artists.items[0].images[0].url);
 
 			},
-			error: function() {
+			error: function(error) {
+				console.log("Error");
+				console.log(error.responseJSON);
+			}
+		});
+	});
+
+	//selector of url                 "img class"
+	$(".js-artist-list").on("click", ".js-albums", function (event) {
+		event.preventDefault();
+
+		alert("Album click");
+
+		var searchedArtist = $(event.currentTarget).data("artist-id");
+
+		console.log(searchedArtist);
+
+		$.ajax({
+			type: "get",
+			url: `https://api.spotify.com/v1/artists/${searchedArtist}/albums`,
+			// data: {
+			// 	q: searchedArtist.replace(" ","+"),
+			// 	type: 'album'
+			// },
+
+			success: function(data) {
+				console.log("Success");
+				console.log(data);
+				showAlbums(data.items);
+				// showAlbums(data.albums.items[0].name);
+				// console.log(data.artists.items[0].name);
+				// shohwAlbum(data.artists.items);
+			},
+			error: function(error) {
 				console.log("Error");
 				console.log(error.responseJSON);
 			}
@@ -37,10 +70,17 @@ function displayInfo (artists) {
     var html = `
       <li>
         <p>Name: ${oneArtist.name}</p>
-        <img src="${oneArtist.images[0].url}">
+        <img src="${oneArtist.images[0].url}" class="js-albums" data-artist-id="${oneArtist.id}">
       </li>`;
     $(".js-artist-list").append(html);
   });
 }
 
-
+function showAlbums (albums) {
+	albums.forEach(function (oneAlbum) {
+		var html = `
+		<li>Album: ${oneAlbum.name}</li>
+		`;
+		$(".js-albums-list").append(html);
+	});
+}
